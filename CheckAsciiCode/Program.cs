d6fd407269c7fcf28890f966cbcb4c892b3ebc99
Tay Zar Soe <s-tayzar@athuman.com>
2023-10-19 13:35:53 +0900

    Initial Commit

---
+++ CheckAsciiCode/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckAsciiCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            //Console.OutputEncoding = Encoding.UTF8;
+            string line;
+            while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                var asciiCodeByte = Encoding.GetEncoding("Windows-1252").GetBytes(line);
+                int asciiCode = asciiCodeByte[0];
+                Console.WriteLine("Your Input is : " + line + " Ascii is : " + asciiCode + " Byte Length is : " + asciiCodeByte.Length);
+            }
+
+        }
+    }
+}
